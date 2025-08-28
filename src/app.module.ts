253b0beaa73e8f10fbe54349031f2f1f698cfaf3
sachinikumanayake <sachikumanayake003@gmail.com>
2025-08-28 14:12:36 +0530

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module'; 
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-crud'),
+    UserModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
